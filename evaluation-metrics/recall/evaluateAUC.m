4672f2f211d8c92a345cb24a21387eda1d97bd06
Joseph KJ <josephkj20@gmail.com>
2018-02-11 15:17:09 +0530

    Changes

--- evaluation-metrics/recall/evaluateAUC.m
+++ evaluation-metrics/recall/evaluateAUC.m
@@ -91,11 +91,11 @@ function evaluateAUC( methods, outputLocation)
          	fprintf('\n****  Continuing ..****\n');
 	end
     end
-    xlim([10, 500]);
+    xlim([10, 1000]);
     ylim([0 1.0]);
     xlabel('# candidates'); ylabel(sprintf('recall at IoU @ %.1f', threshold));
     legend(labels{:}, 'Location', legend_locations{threshold_i});
-    % legendshrink(0.5);
+    legendshrink(0.5);
     legend boxoff;
 %     legend(labels, 'Location', 'SouthEast');
     hei = 10;
